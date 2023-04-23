43dde8099bd6b70fc3d19abdb349d9c5e4dab119
Vincent Edgar Tovar Cortes <vincent.edgren@gmail.com>
2023-04-23 13:24:24 -0600

    Sunset In

---
+++ sunsetFull/sunset/src/data.js
@@ -0,0 +1,64 @@
+const products = [
+  {
+    _id: "1",
+    name: "The Strokes",
+    image:
+      "https://cdn.shopify.com/s/files/1/0536/6084/4186/products/TheStrokesLogo.jpg?v=1664942924&width=360",
+
+    price: 220.50,
+    rating: 4.5,
+    numReviews: 16,
+  },
+  {
+    _id: "2",
+    name: "Tame Impala",
+    image:
+      "https://cdn.shopify.com/s/files/1/0536/6084/4186/products/8_f7dccf34-7e09-4419-82d2-cb017202c1b8.jpg?v=1671172236&width=360",
+
+    price: 280.99,
+    rating: 4.0,
+    numReviews: 17,
+  },
+  {
+    _id: "3",
+    name: "Gorillaz",
+    image:
+      "https://cdn.shopify.com/s/files/1/0536/6084/4186/products/gorillazplayera.jpg?v=1681169325&width=360",
+
+    price: 300.00,
+    rating: 3,
+    numReviews: 23,
+  },
+  {
+    _id: "4",
+    name: "Green Day",
+    image:
+      "https://cdn.shopify.com/s/files/1/0536/6084/4186/products/16_f4b43010-c9b2-4b69-b949-b874a9aaf2f9.jpg?v=1671141482&width=360",
+
+    price: 230.99,
+    rating: 4,
+    numReviews: 25,
+  },
+  {
+    _id: "5",
+    name: "Interpol",
+    image:
+      "https://cdn.shopify.com/s/files/1/0536/6084/4186/products/z.jpg?v=1669794573&width=360",
+
+    price: 300.99,
+    rating: 4.5,
+    numReviews: 29,
+  },
+  {
+    _id: "6",
+    name: "Radiohead",
+    image:
+      "https://cdn.shopify.com/s/files/1/0536/6084/4186/products/2b.jpg?v=1672213875",
+
+    price: 350.50,
+    rating: 4.5,
+    numReviews: 30,
+  },
+];
+
+export default products;
